1698be8438380c60fc08aed5c73aa8759c04b387
br8S <56180503+br8S@users.noreply.github.com>
2021-06-08 14:45:28 -0700

    added on load animation

---
+++ loadAnime.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function() {
+   gsap.to("#load_animation", {
+       autoAlpha: 0, 
+       delay: 2
+   })
+   
+    gsap.to("#load_logo", {
+       rotation: 360,
+       duration: 1
+   })
+
+
+})
