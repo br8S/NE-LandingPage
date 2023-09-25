655df7bda8d2900a1cb73f612c9b8409dfce6f80
Guillermo Mata <guillermo@electriqmarketing.com>
2023-09-24 23:51:57 -0700

    feat: updated w/ parallax elements

--- grained.js
+++ grained.js
@@ -3,7 +3,7 @@ var options = {
     "animate": true,
     "patternWidth": 100,
     "patternHeight": 100,
-    "grainOpacity": 0.46,
+    "grainOpacity": 0.8,
     "grainDensity": 1,
     "grainWidth": 1,
     "grainHeight": 1
